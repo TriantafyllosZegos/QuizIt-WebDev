2a082d70d497721a21c996a1fd16d03728fa35d5
ΤriantafyllosZegos <zegotria@csd.auth.gr>
2022-03-31 13:44:39 +0300

    xroma

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p Style="font-size:100px;">
-          Welcome to <code Style="color:yellow;">Quiz</code><a Style="color:red;">I</a><a Style="color:green;">T</a>.
+          Welcome to <code Style="color:yellow;">Q</code><a Style="color:white;">u</a><a Style="color:orange;">i</a><a Style="color:purple;">z</a><a Style="color:red;">I</a><a Style="color:green;">T</a>.
         </p>
       </header>
     </div>
