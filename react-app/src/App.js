8ddc356072986952ffbbf3c9e970724fc1e2de8b
Aggelos Grigoriou <Aggelosgrig@gmail.com>
2022-03-31 12:22:56 +0300

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p Style="font-size:100px;">
-          Welcome to <code>Quiz</code><a Style="color:red;">I</a><a Style="color:green;">T</a>.
+          Welcome to <code Style="color:yellow;">Quiz</code><a Style="color:red;">I</a><a Style="color:green;">T</a>.
         </p>
       </header>
     </div>
