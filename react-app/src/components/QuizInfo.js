681defb1f73c6d7a68dcccf4c125dee48aefb7e6
Aggelos Grigoriou <Aggelosgrig@gmail.com>
2022-04-08 02:32:24 +0300

    123123

--- react-app/src/components/QuizInfo.js
+++ react-app/src/components/QuizInfo.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
-import ph from '../assets/images/placeholder.png'
+import React, { Component } from "react";
+import ph from "../assets/images/placeholder.png";
 
 export default class QuizInfo extends Component {
   render() {
     return (
-      <div className='QuizInfo'>
+      <div className="QuizInfo">
+        <div className="flex">
+          <h2>Quiz name</h2>
           <img src={ph} />
-          <h1>Quiz name</h1>
-          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ab voluptatem, non aliquam nemo labore magni aut omnis eum? Numquam velit tenetur quo provident natus fugit, voluptas deserunt ad voluptates?</span>
-          <button>Start Quiz</button>
+          <h3>Teacher's name</h3>
+          <span>Class name</span>
+        </div>
+
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ab
+          voluptatem, non aliquam nemo labore magni aut omnis eum? Numquam velit
+          tenetur quo provident natus fugit, voluptas deserunt ad voluptates?
+        </span>
+        <button>Start Quiz</button>
       </div>
-    )
+    );
   }
 }
-   
-
