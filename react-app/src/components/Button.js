a4e9ee7ba1016c88e7da67ed5d7006aa4b5cf456
Aggelos Grigoriou <Aggelosgrig@gmail.com>
2022-04-21 16:51:14 +0300

    modular styles

---
+++ react-app/src/components/Button.js
@@ -0,0 +1,1 @@
+import styles from '../css/components/Button.mobule.css'
--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {GrUser,GrHistory} from 'react-icons/gr'
+import styles from '../css/components/Nav.module.css'
 
 export default function Nav(){
     return (
-      <div className="nav">
-        <h1 className="nav-icon">QuizIT</h1>
-        <div className="nav-icon"><GrUser/></div>
-        <div className="nav-icon"><GrHistory/></div>
+      <div className={styles.nav}>
+        <h1 className={styles.navicon}>QuizIT</h1>
+        <div className={styles.navicon}><GrUser/></div>
+        <div className={styles.navicon}><GrHistory/></div>
       </div>
     );
   }
