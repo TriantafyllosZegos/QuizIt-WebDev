5be49f18477717a2e26216f3eb51a8be15e07eaa
ΤriantafyllosZegos <zegotria@csd.auth.gr>
2022-04-21 17:06:12 +0300

    button

--- react-app/src/components/Button.jsx
+++ react-app/src/components/Button.jsx
@@ -1,1 +1,36 @@
-import styles from '../css/components/Button.mobule.css'
+import React, { Component }  from "react"
+
+const STYLES = [
+"btn--primary--solid",
+"btn--name1--solid",
+"btn--name2--solid",
+"btn--name3--solid",
+"btn--primary--outline",
+"btn--name1--outline",
+"btn--name2--outline",
+"btn--name3--outline",
+];
+
+const SIZES = ["btn--medium","btn--large"];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSyze
+
+}) => {
+
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+    ? buttonStyle
+     : STYLES[0];
+
+    
+    return (
+        <button onClick={onClick} type={type}>
+            {children}
+        </button>
+
+    );
+};
