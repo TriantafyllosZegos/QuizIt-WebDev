f451e68775205c899b1a0f24945be967390bbdcf
Aggelos Grigoriou <Aggelosgrig@gmail.com>
2022-04-28 18:52:11 +0300

    tets

--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import {GrUser,GrHistory} from 'react-icons/gr'
 import styles from '../css/components/Nav.module.css'
 import {NavLink} from 'react-router-dom'
+import logo from "../assets/icons/logo.png";
 
 export default function Nav(){
     return (
       <div className={styles.nav}>
-        <NavLink className={styles.navicon} to="student">QuizIT</NavLink>
+        <NavLink className={styles.navicon} to="student"><img src={logo} alt="logo" /></NavLink>
         <NavLink className={styles.navicon} to="student/profile"><GrUser/></NavLink>
         <NavLink className={styles.navicon} to="student/history"><GrHistory/></NavLink>
       </div>
